a5aec9477a8c64b27019c5fca44af806cbda3397
muchristian <mucyochristian2@gmail.com>
2020-11-10 19:27:18 +0200

    production db connect

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,3 +1,7 @@
+const urlParser = require('url');
+const url = 'postgres://pbtasqbfxoatwa:e8d10a0739a758ae89ff3fac38de6cc22ab98da6f2403c8cc8ba018fe3cbd08f@ec2-54-159-112-44.compute-1.amazonaws.com:5432/d3lvoavj269lid'
+const q = urlParser.parse(url, true);
+
 module.exports = {
   development: {
     username: "postgres",
@@ -14,10 +18,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: "postgres",
-    password: "chris32",
-    database: "database_production",
-    host: "127.0.0.1",
+    username: q.user,
+    password: q.pass,
+    database: d.path,
+    host: q.host,
     dialect: "postgres",
   },
 };
